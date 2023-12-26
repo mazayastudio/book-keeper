8d7953349705dcad944991590c943bcbbd586a7c
sayudhalw <mazaya.studio@outlook.com>
2023-12-26 07:21:51 +0700

    Add deleteBookmark function to remove bookmarks and update localStorage

--- script.js
+++ script.js
@@ -35,6 +35,18 @@ function validate(nameValue, urlValue) {
 	return true;
 }
 
+// Delete Bookmark
+function deleteBookmark(url) {
+	bookmarks.forEach((bookmark, i) => {
+		if (bookmark.url === url) {
+			bookmarks.splice(i, 1);
+		}
+	});
+	// Update bookmarks array in localStorage, re-populate DOM
+	localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+	fetchBookmarks();
+}
+
 // Build Bookmarks DOM
 function buildBookmarks() {
 	// Remove all bookmark elements
